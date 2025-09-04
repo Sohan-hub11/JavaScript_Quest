05ce47b53404f6e687b41e0e2884d2d9ccc08dc6
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-09-04 21:36:58 +0530

    Update app.js

--- DOM/app.js
+++ DOM/app.js
@@ -27,3 +27,25 @@ console.dir(document.querySelector(".oldimg"));
 console.dir(document.querySelector("div a"));
 
 console.dir(document.querySelectorAll("h1"));
+
+
+//Setting Content in Objects
+
+let para = document.querySelectorAll("p");
+console.dir(para);
+para[1].innerText;
+// 'In 1962, with the success of the Fantastic Four, Marvel Comics editor and head writer Stan Lee was casting for a new superhero idea. He said the idea for Spider-Man arose from a surge in teenage demand for comic books and the desire to create a character with whom teens could identify. As with Fantastic Four, Lee saw Spider-Man as an opportunity to "get out of his system" what he felt was missing in comic books. In his autobiography, Lee cites the non-superhuman pulp magazine crime fighter the Spider as a great influence, and in a multitude of print and video interviews, Lee stated he was further inspired by seeing a spider climb up a wall—adding in his autobiography that he has told that story so often he has become unsure of whether or not this is true.'
+para[1].innerHTML;
+// '\n      In 1962, with the success of the Fantastic Four, Marvel Comics editor and\n      head writer Stan Lee was casting for a new superhero idea. He said the\n      idea for Spider-Man arose from a surge in teenage demand for comic books\n      and the desire to create a character with whom teens could identify. As\n      with Fantastic Four, Lee saw Spider-Man as an opportunity to "get out of\n      his system" what he felt was missing in comic books. In his autobiography,\n      Lee cites the non-superhuman pulp magazine crime fighter the Spider as a\n      great influence, and in a multitude of print and video interviews, Lee\n      stated he was further inspired by seeing a spider climb up a wall—adding\n      in his autobiography that he has told that story so often he has become\n      unsure of whether or not this is true.\n    '
+para[1].textContent;
+// '\n      In 1962, with the success of the Fantastic Four, Marvel Comics editor and\n      head writer Stan Lee was casting for a new superhero idea. He said the\n      idea for Spider-Man arose from a surge in teenage demand for comic books\n      and the desire to create a character with whom teens could identify. As\n      with Fantastic Four, Lee saw Spider-Man as an opportunity to "get out of\n      his system" what he felt was missing in comic books. In his autobiography,\n      Lee cites the non-superhuman pulp magazine crime fighter the Spider as a\n      great influence, and in a multitude of print and video interviews, Lee\n      stated he was further inspired by seeing a spider climb up a wall—adding\n      in his autobiography that he has told that story so often he has become\n      unsure of whether or not this is true.\n    '
+para[1].innerText = "My name is Sohan Samanta";
+// 'My name is Sohan Samanta'
+para[1].innerHTML = "My name is <b>Sohan Samanta</b>";
+
+let heading = document.querySelector("h1");
+heading.innerHTML
+//'Spider Man'
+heading.innerText
+//'Spider Man'
+heading.innerHTML = `<u>${heading.innerText}</u>`; //changing th text to underlines.
