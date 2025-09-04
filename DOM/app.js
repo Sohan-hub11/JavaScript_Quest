ca8fb6925f467e01c245020ff37d9d06160f9edf
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-09-04 20:50:27 +0530

    Add files via upload

---
+++ DOM/app.js
@@ -0,0 +1,16 @@
+//Selecting Elements -->
+
+//console.dir(document);
+
+document.getElementById("description"); //get element by Id.
+//console.dir(document.getElementById("description")); //to get id as object.
+
+document.getElementsByClassName("oldImg"); //get element by class name.
+
+let oimg = document.getElementsByClassName("oldImg");
+for( let i = 0; i< oimg.length; i++){
+    //console.log(oimg[i]);
+    //console.log(oimg[i].src);
+    oimg[i].src="assets/spiderman_img.png";
+    console.log(`The value of old img is changed to. ${i}`);
+}
