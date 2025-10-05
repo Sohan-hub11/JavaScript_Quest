96b02aaf12b9ec0ad587b28143bc92e2bc82cd21
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-10-05 22:07:03 +0530

    Update 07_AssignmentQue3.js

---
+++ 07_AssignmentQue3.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program to get the first n elements of an array. [n can be any positive number].
+
+arr = [7, 9, 0, -2];
+n = 3;
+let ans = arr.slice(0,n)
+console.log(ans)
+
+// Write a JavaScript program to get the last n elements of an array.[n can be any positive number].
