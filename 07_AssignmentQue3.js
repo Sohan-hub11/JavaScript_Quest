ef4598443f3f0f79664286cc84293721896e0f9c
Sohan Samanta <118916618+Sohan-hub11@users.noreply.github.com>
2025-10-05 22:17:26 +0530

    Update 07_AssignmentQue3.js

--- 07_AssignmentQue3.js
+++ 07_AssignmentQue3.js
@@ -3,6 +3,8 @@
 arr = [7, 9, 0, -2];
 n = 3;
 let ans = arr.slice(0,n)
-console.log(ans)
+//console.log(ans)
 
 // Write a JavaScript program to get the last n elements of an array.[n can be any positive number].
+let ans1 = arr.slice(arr.length - n)
+console.log(ans1)
